38ea6da5e89d7dae34350f53d1f1a74248021d4b
Anastasia Han <anastasia.han@yahoo.com>
2022-05-15 18:53:42 -0400

    Inventory and Item.js added

---
+++ src/js/item.js
@@ -0,0 +1,89 @@
+export default class Item {
+  constructor (name, str, dex, int, value) {
+    this.name = name;
+    this.str = str;
+    this.dex = dex;
+    this.int = int;
+    this.value = value;
+  }
+
+  ToString() {
+    return '${this.name}: [str: $Pthis.str}, dex: ${this.dex}, int: ${this.int}] ${this.value *1.5}dollars';
+  }
+
+  static CreateItem(level) {
+    let focus = Math.floor(Math.random() * 3 );
+    let str = 0;
+    let dex = 0;
+    let int = 0;
+    let className = "";
+
+    switch (focus) {
+      case 0: 
+        str = Math.floor((Math.random() * 5) + level * 2 + 3);
+        dex = Math.floor((Math.random() * 5) + level + 1);
+        int = Math.floor((Math.random() * 3));
+        className += weaponNames.class.warrior[Math.floor(Math.random() * 4)];
+        break;
+      case 1: 
+        str = Math.floor((Math.random() * 5) + level + 1);
+        dex = Math.floor((Math.random() * 5) + level * 2 + 3);
+        int = Math.floor((Math.random() * 3));
+        className += weaponNames.class.assassin[Math.floor(Math.random() * 4)];
+        break;
+      case 2:
+        str = Math.floor((Math.random() * 3));
+        dex = Math.floor((Math.random() * 5) + level + 1);
+        int = Math.floor((Math.random() * 5) + level * 2 + 3);
+        className += weaponNames.class.wizard[Math.floor(Math.random() * 4)];
+        break;
+    }
+
+    let name = weaponnames.prefix[Math.floor(Math.random() *9)] + " " + className + " of " +weaponNames.suffix[Math.floor(Math.random() * 8)];
+
+    return new Item(name, str, dex, int, 10 * level);
+  }
+}
+
+const weaponNames = {
+  "class": {
+    "honda": [
+      "keyfob",
+      "fender eliminator",
+      "exhaust",
+    ],
+    "suzuki" : [
+      "keychain",
+      "bar end mirror",
+      "axle protector"
+    ],
+    "ducati" : [
+      "desmo valve",
+      "red helmet",
+      "oil pan"
+    ],
+    "triumph" : [
+      "tophat",
+      "mustache tattoo",
+      "comfort seat"
+    ]
+  },
+  "prefix": [
+    "shiny",
+    "leaking",
+    "OEM",
+    "aftermarket",
+    "second-hand",
+    "HP Boost",
+    "Torqueboosting",
+    "Extra loud"
+  ],
+  "suffix" : [
+    "destruction",
+    "snobbery",
+    "chaos",
+    "slipperiness",
+    "speed",
+    "noise"
+  ]
+};
