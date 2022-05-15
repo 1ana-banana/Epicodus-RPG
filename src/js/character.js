76424a42d23763dd62f883dc29c7d3c5da47eeab
Anastasia Han <anastasia.han@yahoo.com>
2022-05-15 18:10:26 -0400

    character.js completed

---
+++ src/js/character.js
@@ -0,0 +1,46 @@
+import Inventory from "./inventory.js";
+
+export default class Character {
+  constructor(name, charClass) {
+    this.name = name;
+    this.charClass = charClass;
+    this.hp = this.charClass.str + 50;
+    this.money = 10;
+    this.inventory = new Inventory (5);
+    this.equipment = [];
+  }
+
+  LevelUp() {
+    this.charClass.LevelUp();
+    this.hp = this.charClass.str +50;
+  }
+
+  EquipItem(item) {
+    if (this.equipment.length >= 2) {
+      return "Cannot be equipped";
+    } else {
+      const invItem = this.inventory.RemoveItem(item);
+      if (invItem) {
+        this.equipment.push(invItem);
+        this.charClass.AddItem(invItem);
+        this.hp = this.charClass.str + 50;
+        return "Success!";
+      } else {
+        return "The tank bag is empty.";
+      }
+    }
+  }
+
+  RemoveItem(targetItem) {
+    let index = this.equipment.findIndex(item => item.ToString() === targetItem);
+    if (index !== -1 && !this.inventory.IsFull()) {
+      const equipItem = this.equipment.splice(index, 1)[0];
+      this.charClass.RemoveItem(equipItem);
+      this.hp = this.charClass.str +5;
+      this.inventory.AddItem(equipItem);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
